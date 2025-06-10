7c6d48940dc1fa4b1027ab7cbfa78c15b0aa4aa6
iolav <120427939+iolav@users.noreply.github.com>
2025-06-10 02:58:19 -0500

    Add project files.

---
+++ Pigface_ExplosiveLMG/GunPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Pigface_ExplosiveLMG.Patches
+{
+    [HarmonyPatch(typeof(Gun), "OnGunShot")]
+    internal class GunPatch
+    {
+        static void Postfix(Gun __instance)
+        {
+            NailBombInstance[] BombInstances = Resources.FindObjectsOfTypeAll<NailBombInstance>();
+
+            NailBombInstance NewBomb = UnityEngine.Object.Instantiate(BombInstances[0], __instance.muzzlePoint.position, __instance.transform.rotation);
+
+            float Force = 100f;
+            NewBomb.GetComponent<Rigidbody>().AddForce(__instance.muzzlePoint.forward * Force * 1.5f + __instance.muzzlePoint.up * Force / 8f, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Pigface_ExplosiveLMG/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Pigface_ExplosiveLMG
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string PluginGUID = "iolav.Pigface_ExplosiveLMG";
+        private const string PluginName = "Explosive LMG";
+        private const string PluginVersion = "1.0.0";
+
+        private readonly Harmony MyHarmony = new Harmony(PluginGUID);
+
+        public static ManualLogSource? Log;
+
+        private void Awake()
+        {
+            Log = Logger;
+            Logger.LogInfo($"Loaded {PluginGUID}");
+
+            MyHarmony.PatchAll();
+        }
+    }
+}
